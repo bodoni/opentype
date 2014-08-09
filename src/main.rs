43da3c4d06f1d513da2c8cf64298dcac9f4f8aca
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-08-09 09:20:49 +0200

    A little step forward in main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,45 @@
 #![crate_name = "benton"]
 
 use std::os;
+use std::io::File;
 
 fn main() {
     let arguments = os::args();
 
     if arguments.len() != 2 {
-        println!("Usage: benton <file>");
+        println!("Usage: {} <file>", arguments[0]);
         return;
     }
 
-    let ref filename = arguments[1];
+    let filename: &str = arguments[1].as_slice();
 
     println!("Filename: {}", filename);
+
+    let mut reader = match File::open(&Path::new(filename)) {
+        Ok(file) => box file as Box<Reader>,
+        Err(_) => {
+            println!("Cannot open the file.");
+            return;
+        }
+    };
+
+    let mut buffer = [0, ..4];
+
+    let tag = match reader.read(buffer) {
+        Ok(n) => {
+            match std::str::from_utf8(buffer.slice_to(n)) {
+                Some(string) => string,
+                None => {
+                    println!("Cannot read the file.")
+                    return;
+                }
+            }
+        },
+        Err(_) => {
+            println!("Cannot read the file.");
+            return;
+        }
+    };
+
+    println!("Tag: {}", tag);
 }

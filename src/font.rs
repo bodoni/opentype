6698541f0ad44d0057aa5c70979b5dfc9fd3a374
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-08-11 13:29:54 -0400

    Introduce verify_and_position!

--- src/font.rs
+++ src/font.rs
@@ -22,7 +22,7 @@ pub struct Font {
 macro_rules! tag(
     ($value:expr) => (unsafe {
         mem::transmute::<_, [u8; 4]>($value)
-    })
+    });
 );
 
 #[cfg(target_endian = "little")]
@@ -31,19 +31,27 @@ macro_rules! tag(
         let mut value = mem::transmute::<_, [u8; 4]>($value);
         value.reverse();
         value
-    })
+    });
+);
+
+macro_rules! verify_and_position(
+    ($record:ident, $band:ident, $table:expr, $process:expr) => ({
+        if !try!($record.check($band, $process)) {
+            raise!(concat!("the ", $table, " is corrupted"));
+        }
+        try!($band.jump($record.offset as u64));
+    });
+    ($record:ident, $band:ident, $table:expr) => (
+        verify_and_position!($record, $band, $table, |_, word| word);
+    );
 );
 
 impl Font {
     pub fn read<T: Read + Seek>(reader: &mut T) -> Result<Font> {
         macro_rules! some(
-            ($option:expr, $object:expr, plural) => (match $option {
+            ($option:expr, $table:expr) => (match $option {
                 Some(value) => value,
-                _ => raise!(concat!($object, " are missing")),
-            });
-            ($option:expr, $object:expr) => (match $option {
-                Some(value) => value,
-                _ => raise!(concat!($object, " is missing")),
+                _ => raise!(concat!("the ", $table, " is missing")),
             });
         );
 
@@ -73,8 +81,8 @@ impl Font {
                 b"head" => font_header = Some(try!(read_font_header(reader, record))),
                 b"hhea" => horizontal_header = Some(try!(read_horizontal_header(reader, record))),
                 b"hmtx" => {
-                    let header = some!(horizontal_header.as_ref(), "the horizontal header");
-                    let profile = some!(maximum_profile.as_ref(), "the maximum profile");
+                    let header = some!(horizontal_header.as_ref(), "horizontal header");
+                    let profile = some!(maximum_profile.as_ref(), "maximum profile");
                     horizontal_metrics = Some(try!(read_horizontal_metrics(reader, record, header,
                                                                            profile)));
                 },
@@ -87,13 +95,13 @@ impl Font {
 
         Ok(Font {
             offset_table: offset_table,
-            char_mapping: some!(char_mapping, "the character-to-glyph mapping"),
-            font_header: some!(font_header, "the font header"),
-            horizontal_header: some!(horizontal_header, "the horizontal header"),
-            horizontal_metrics: some!(horizontal_metrics, "the horizontal metrics", plural),
-            maximum_profile: some!(maximum_profile, "the maximum profile"),
-            postscript: some!(postscript, "the PostScript information"),
-            windows_metrics: some!(windows_metrics, "the OS/2 and Windows metrics", plural),
+            char_mapping: some!(char_mapping, "character-to-glyph mapping"),
+            font_header: some!(font_header, "font header"),
+            horizontal_header: some!(horizontal_header, "horizontal header"),
+            horizontal_metrics: some!(horizontal_metrics, "horizontal metrics"),
+            maximum_profile: some!(maximum_profile, "maximum profile"),
+            postscript: some!(postscript, "PostScript information"),
+            windows_metrics: some!(windows_metrics, "OS/2 and Windows metrics"),
         })
     }
 }
@@ -113,14 +121,10 @@ fn read_offset_table<T: Band>(band: &mut T) -> Result<OffsetTable> {
     Ok(OffsetTable { header: header, records: records })
 }
 
-fn read_char_mapping<T: Band>(band: &mut T, table: &OffsetTableRecord) -> Result<CharMapping> {
+fn read_char_mapping<T: Band>(band: &mut T, record: &OffsetTableRecord) -> Result<CharMapping> {
     const VERSION_0_0: USHORT = 0;
 
-    if !try!(table.check(band, |_, chunk| chunk)) {
-        raise!("the character-to-glyph mapping is corrupted");
-    }
-    try!(band.jump(table.offset as u64));
-
+    verify_and_position!(record, band, "character-to-glyph mapping");
     let header = try!(CharMappingHeader::read(band));
     if header.version != VERSION_0_0 {
         raise!("the format of the character-to-glyph mapping header is not supported");
@@ -131,7 +135,7 @@ fn read_char_mapping<T: Band>(band: &mut T, table: &OffsetTableRecord) -> Result
     }
     let mut encodings = vec![];
     for encoding in records.iter() {
-        try!(band.jump(table.offset as u64 + encoding.offset as u64));
+        try!(band.jump(record.offset as u64 + encoding.offset as u64));
         encodings.push(match try!(band.peek::<USHORT>()) {
             4 => CharMappingEncoding::Format4(try!(Value::read(band))),
             6 => CharMappingEncoding::Format6(try!(Value::read(band))),
@@ -146,11 +150,7 @@ fn read_font_header<T: Band>(band: &mut T, record: &OffsetTableRecord) -> Result
     const MAGIC_NUMBER: ULONG = 0x5F0F3CF5;
     const VERSION_1_0: Fixed = Fixed(0x00010000);
 
-    if !try!(record.check(band, |i, chunk| if i == 2 { 0 } else { chunk })) {
-        raise!("the font header is corrupted");
-    }
-    try!(band.jump(record.offset as u64));
-
+    verify_and_position!(record, band, "font header", |i, word| if i == 2 { 0 } else { word });
     let table = try!(FontHeader::read(band));
     if table.version != VERSION_1_0 {
         raise!("the format of the font header is not supported");
@@ -167,11 +167,7 @@ fn read_horizontal_header<T: Band>(band: &mut T, record: &OffsetTableRecord)
 
     const VERSION_1_0: Fixed = Fixed(0x00010000);
 
-    if !try!(record.check(band, |_, chunk| chunk)) {
-        raise!("the horizontal header is corrupted");
-    }
-    try!(band.jump(record.offset as u64));
-
+    verify_and_position!(record, band, "horizontal header");
     let table = try!(HorizontalHeader::read(band));
     if table.version != VERSION_1_0 {
         raise!("the format of the horizontal header is not supported");
@@ -184,11 +180,7 @@ fn read_horizontal_metrics<T: Band>(band: &mut T, record: &OffsetTableRecord,
                                     header: &HorizontalHeader, profile: &MaximumProfile)
                                     -> Result<HorizontalMetrics> {
 
-    if !try!(record.check(band, |_, chunk| chunk)) {
-        raise!("the horizontal metrics are corrupted");
-    }
-    try!(band.jump(record.offset as u64));
-
+    verify_and_position!(record, band, "horizontal metrics");
     Ok(try!(HorizontalMetrics::read(band, header, profile)))
 }
 
@@ -198,11 +190,7 @@ fn read_maximum_profile<T: Band>(band: &mut T, record: &OffsetTableRecord)
     const VERSION_0_5: Fixed = Fixed(0x00005000);
     const VERSION_1_0: Fixed = Fixed(0x00010000);
 
-    if !try!(record.check(band, |_, chunk| chunk)) {
-        raise!("the maximum profile is corrupted");
-    }
-    try!(band.jump(record.offset as u64));
-
+    verify_and_position!(record, band, "maximum profile");
     Ok(match try!(band.peek::<Fixed>()) {
         VERSION_0_5 => MaximumProfile::Version05(try!(Value::read(band))),
         VERSION_1_0 => MaximumProfile::Version10(try!(Value::read(band))),
@@ -214,11 +202,7 @@ fn read_postscript<T: Band>(band: &mut T, record: &OffsetTableRecord) -> Result<
     const VERSION_1_0: Fixed = Fixed(0x00010000);
     const VERSION_3_0: Fixed = Fixed(0x00030000);
 
-    if !try!(record.check(band, |_, chunk| chunk)) {
-        raise!("the PostScript information is corrupted");
-    }
-    try!(band.jump(record.offset as u64));
-
+    verify_and_position!(record, band, "PostScript information");
     Ok(match try!(band.peek::<Fixed>()) {
         VERSION_1_0 => PostScript::Version10(try!(Value::read(band))),
         VERSION_3_0 => PostScript::Version30(try!(Value::read(band))),
@@ -229,11 +213,7 @@ fn read_postscript<T: Band>(band: &mut T, record: &OffsetTableRecord) -> Result<
 fn read_windows_metrics<T: Band>(band: &mut T, record: &OffsetTableRecord)
                                  -> Result<WindowsMetrics> {
 
-    if !try!(record.check(band, |_, chunk| chunk)) {
-        raise!("the OS/2 and Windows metrics are corrupted");
-    }
-    try!(band.jump(record.offset as u64));
-
+    verify_and_position!(record, band, "OS/2 and Windows metrics");
     Ok(match try!(band.peek::<USHORT>()) {
         3 => WindowsMetrics::Version3(try!(Value::read(band))),
         5 => WindowsMetrics::Version5(try!(Value::read(band))),

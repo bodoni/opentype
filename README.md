fb95b3e68e816c45770469c303a0826432242db8
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-08-11 21:41:33 -0400

    Add an example

--- README.md
+++ README.md
@@ -4,6 +4,30 @@ The package provides a parser for OpenType fonts.
 
 ## [Documentation][doc]
 
+## Example
+
+```rust
+use opentype::Font;
+use opentype::table::NamingTable;
+use std::fs::File;
+
+let path = "SourceSerifPro-Regular.otf";
+# let path = "tests/fixtures/SourceSerifPro-Regular.otf";
+let mut file = File::open(path).unwrap();
+let font = Font::read(&mut file).unwrap();
+
+assert_eq!(font.font_header.unitsPerEm, 1000);
+assert_eq!(font.horizontal_header.Ascender, 918);
+
+let strings = match font.naming_table {
+    NamingTable::Format0(ref table) => table.strings().unwrap(),
+    _ => unreachable!(),
+};
+
+assert_eq!(&strings[1], "Source Serif Pro");
+assert_eq!(&strings[9], "Frank Grießhammer");
+```
+
 ## Contributing
 
 1. Fork the project.
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,28 @@
 //! A parser for OpenType fonts.
+//!
+//! ## Example
+//!
+//! ```
+//! use opentype::Font;
+//! use opentype::table::NamingTable;
+//! use std::fs::File;
+//!
+//! let path = "SourceSerifPro-Regular.otf";
+//! # let path = "tests/fixtures/SourceSerifPro-Regular.otf";
+//! let mut file = File::open(path).unwrap();
+//! let font = Font::read(&mut file).unwrap();
+//!
+//! assert_eq!(font.font_header.unitsPerEm, 1000);
+//! assert_eq!(font.horizontal_header.Ascender, 918);
+//!
+//! let strings = match font.naming_table {
+//!     NamingTable::Format0(ref table) => table.strings().unwrap(),
+//!     _ => unreachable!(),
+//! };
+//!
+//! assert_eq!(&strings[1], "Source Serif Pro");
+//! assert_eq!(&strings[9], "Frank Grießhammer");
+//! ```
 
 /// An error.
 pub type Error = std::io::Error;

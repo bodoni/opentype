3d88d96a59178bbf7fa0de1caa44029586f2644d
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-08-28 14:08:14 +0200

    Update the usage of truetype

--- src/font.rs
+++ src/font.rs
@@ -19,12 +19,6 @@ pub struct Font {
     pub windows_metrics: Option<WindowsMetrics>,
 }
 
-macro_rules! tag(
-    ($value:expr) => (unsafe {
-        mem::transmute::<_, [u8; 4]>(u32::from_be(mem::transmute($value)))
-    });
-);
-
 macro_rules! checksum_and_jump(
     ($record:ident, $tape:ident, $process:expr) => ({
         if !try!($record.checksum($tape, $process)) {
@@ -44,14 +38,14 @@ impl Font {
             ($records:expr) => ({
                 let mut records = $records.iter().collect::<Vec<_>>();
                 records.sort_by(|one, two| {
-                    priority(&tag!(one.tag)).cmp(&priority(&tag!(two.tag)))
+                    priority(Tag(one.tag)).cmp(&priority(Tag(two.tag)))
                 });
                 records
             });
         );
 
         let mut font = Font { offset_table: try!(Value::read(tape)), .. Font::default() };
-        if &tag!(font.offset_table.header.version) != b"OTTO" {
+        if Tag::from(font.offset_table.header.version) != Tag::from(b"OTTO") {
             raise!("the font format is invalid");
         }
 
@@ -63,7 +57,7 @@ impl Font {
                 });
                 ($field:ident) => (set!($field, Value::read(tape)));
             );
-            match &tag!(record.tag) {
+            match &Tag(record.tag).into() {
                 b"cmap" => set!(char_mapping),
                 b"head" => {
                     checksum_and_jump!(record, tape, |i, word| if i == 2 { 0 } else { word });
@@ -93,18 +87,18 @@ impl Font {
     }
 }
 
-fn priority(tag: &[u8; 4]) -> usize {
+fn priority(tag: Tag) -> usize {
     use std::collections::HashMap;
     use std::sync::{Once, ONCE_INIT};
 
     unsafe {
-        static mut PRIORITY: *const HashMap<[u8; 4], usize> = 0 as *const _;
+        static mut PRIORITY: *const HashMap<Tag, usize> = 0 as *const _;
         static ONCE: Once = ONCE_INIT;
         ONCE.call_once(|| {
-            let mut map: HashMap<[u8; 4], usize> = HashMap::new();
-            map.insert(*b"hmtx", 42);
+            let mut map: HashMap<Tag, usize> = HashMap::new();
+            map.insert(Tag::from(b"hmtx"), 42);
             PRIORITY = mem::transmute(Box::new(map));
         });
-        *(&*PRIORITY).get(tag).unwrap_or(&0)
+        *(&*PRIORITY).get(&tag).unwrap_or(&0)
     }
 }

8f720e6929395731d8e48bb3e4c6937eeb619dcd
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-12-31 14:17:57 +0100

    Update truetype

--- src/lib.rs
+++ src/lib.rs
@@ -13,7 +13,7 @@
 //! # let path = "tests/fixtures/SourceSerifPro-Regular.otf";
 //! let file = File::open(path).unwrap();
 //!
-//! assert_eq!(file.font_header.as_ref().unwrap().unitsPerEm, 1000);
+//! assert_eq!(file.font_header.as_ref().unwrap().units_per_em, 1000);
 //! assert_eq!(file.horizontal_header.as_ref().unwrap().ascender, 918);
 //! let strings = match file.naming_table {
 //!     Some(NamingTable::Format0(ref table)) => table.strings().unwrap(),

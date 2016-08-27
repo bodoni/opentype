f9a5310272f2a7612dfd1baebc40183a99dcc814
Ivan Ukhov <ivan.ukhov@gmail.com>
2016-08-27 12:48:13 +0200

    Reorganize GlyphDefinition

--- src/glyph_definition/table.rs
+++ src/glyph_definition/table.rs
@@ -4,9 +4,45 @@ use glyph_definition::{Attachments, Ligatures, Marks};
 use glyph_transformation::Class;
 
 table! {
+    @position
     #[doc = "A glyph-definition table."]
     pub GlyphDefinition {
         header (Header),
+
+        glyph_class (Option<Class>) |this, tape, position| {
+            jump_take_maybe!(tape, position, match this.header {
+                Header::Version1(ref header) => header.glyph_class_offset,
+                Header::Version12(ref header) => header.glyph_class_offset,
+            })
+        },
+
+        attachments (Option<Attachments>) |this, tape, position| {
+            jump_take_maybe!(tape, position, match this.header {
+                Header::Version1(ref header) => header.attachments_offset,
+                Header::Version12(ref header) => header.attachments_offset,
+            })
+        },
+
+        ligatures (Option<Ligatures>) |this, tape, position| {
+            jump_take_maybe!(tape, position, match this.header {
+                Header::Version1(ref header) => header.ligatures_offset,
+                Header::Version12(ref header) => header.ligatures_offset,
+            })
+        },
+
+        mark_class (Option<Class>) |this, tape, position| {
+            jump_take_maybe!(tape, position, match this.header {
+                Header::Version1(ref header) => header.mark_class_offset,
+                Header::Version12(ref header) => header.mark_class_offset,
+            })
+        },
+
+        marks (Option<Marks>) |this, tape, position| {
+            jump_take_maybe!(tape, position, match this.header {
+                Header::Version12(ref header) => header.marks_offset,
+                _ => 0,
+            })
+        },
     }
 }
 
@@ -20,36 +56,20 @@ pub enum Header {
 }
 
 table! {
-    @position
     #[doc = "The header of a glyph-definition table of version 0x00010000."]
+    #[derive(Copy)]
     pub Header1 {
         version            (u32) = { 0x00010000 }, // Version
         glyph_class_offset (u16), // GlyphClassDef
         attachments_offset (u16), // AttachList
         ligatures_offset   (u16), // LigCaretList
         mark_class_offset  (u16), // MarkAttachClassDef
-
-        glyph_class (Option<Class>) |this, tape, position| {
-            jump_take_maybe!(tape, position, this.glyph_class_offset)
-        },
-
-        attachments (Option<Attachments>) |this, tape, position| {
-            jump_take_maybe!(tape, position, this.attachments_offset)
-        },
-
-        ligatures (Option<Ligatures>) |this, tape, position| {
-            jump_take_maybe!(tape, position, this.ligatures_offset)
-        },
-
-        mark_class (Option<Class>) |this, tape, position| {
-            jump_take_maybe!(tape, position, this.mark_class_offset)
-        },
     }
 }
 
 table! {
-    @position
     #[doc = "The header of a glyph-definition table of version 0x00010002."]
+    #[derive(Copy)]
     pub Header12 {
         version            (u32) = { 0x00010002 },
         glyph_class_offset (u16), // GlyphClassDef
@@ -57,26 +77,6 @@ table! {
         ligatures_offset   (u16), // LigCaretList
         mark_class_offset  (u16), // MarkAttachClassDef
         marks_offset       (u16), // MarkGlyphSetsDef
-
-        glyph_class (Option<Class>) |this, tape, position| {
-            jump_take_maybe!(tape, position, this.glyph_class_offset)
-        },
-
-        attachments (Option<Attachments>) |this, tape, position| {
-            jump_take_maybe!(tape, position, this.attachments_offset)
-        },
-
-        ligatures (Option<Ligatures>) |this, tape, position| {
-            jump_take_maybe!(tape, position, this.ligatures_offset)
-        },
-
-        mark_class (Option<Class>) |this, tape, position| {
-            jump_take_maybe!(tape, position, this.mark_class_offset)
-        },
-
-        marks (Option<Marks>) |this, tape, position| {
-            jump_take_maybe!(tape, position, this.marks_offset)
-        },
     }
 }
 

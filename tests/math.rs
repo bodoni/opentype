49fe301db724ccfb08285ded0c074813ea8a9dfa
Christopher Breeden <github@u.breeden.cc>
2016-10-08 20:11:37 -0500

    Update mathematics tests for new namespaces.

--- tests/math.rs
+++ tests/math.rs
@@ -1,14 +1,10 @@
-use opentype::math_table::Math;
-use opentype::math_table::constants::{ Percentage, DesignUnits };
+use opentype::mathematics::Mathematics;
 use truetype::Value;
 
 #[test]
 fn table() {
-    let math: Math = ok!(Value::read(&mut setup!(MATH, "MATH")));
+    let math: Mathematics = ok!(Value::read(&mut setup!(MATH, "MATH")));
     assert_eq!(math.header.constants_offset, 10);
     assert_eq!(math.header.glyph_info_offset, 224);
     assert_eq!(math.header.variants_offset, 17034);
-
-    assert_eq!(math.constants.axis_height.value, DesignUnits(250));
-    assert_eq!(math.constants.script_percent_scale_down, Percentage(70));
 }

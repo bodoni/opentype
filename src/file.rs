ebbd529ffa9c3848a7f6a7a51767ec61c98d3f75
Ivan Ukhov <ivan.ukhov@gmail.com>
2022-12-27 11:27:34 +0100

    Make a cosmetic adjustment

--- src/file.rs
+++ src/file.rs
@@ -14,7 +14,7 @@ impl File {
     /// Read a file.
     pub fn read<T: Read + Seek>(tape: &mut T) -> Result<File> {
         if Tag::from(Tape::peek::<u32>(tape)?) == Tag(*b"ttcf") {
-            raise!("TrueType collections are not supported yet");
+            raise!("found a TrueType collection, which is not supported yet");
         }
         Ok(File {
             fonts: vec![Font::read(tape)?],

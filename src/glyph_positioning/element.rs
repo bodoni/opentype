9071d3cd6e7e61267cd0bbbaedb2d750cdfe46c2
Ivan Ukhov <ivan.ukhov@gmail.com>
2023-08-19 07:33:33 +0200

    Rewrite the logic of parsing positioning elements

--- src/glyph_positioning/element.rs
+++ src/glyph_positioning/element.rs
@@ -60,6 +60,8 @@ table! {
     #[doc = "A base attachment."]
     pub Base { // BaseRecord
         anchor_offsets (Vec<u16>), // baseAnchorOffsets
+
+        anchors (Vec<Option<Anchor>>),
     }
 }
 
@@ -69,8 +71,6 @@ table! {
     pub Bases { // BaseArray
         count   (u16      ), // baseCount
         records (Vec<Base>), // baseRecords
-
-        anchors (Vec<Vec<Option<Anchor>>>),
     }
 }
 
@@ -79,6 +79,8 @@ table! {
     #[doc = "A component attachment."]
     pub Component { // ComponentRecord
         anchor_offsets (Vec<u16>), // ligatureAnchorOffsets
+
+        anchors (Vec<Option<Anchor>>),
     }
 }
 
@@ -99,15 +101,17 @@ table! {
         offsets (Vec<u16>), // ligatureAttachOffsets
 
         records (Vec<Ligature>),
-        anchors (Vec<Vec<Vec<Option<Anchor>>>>),
     }
 }
 
 table! {
+    @define
     #[doc = "A mark attachment in format 1."]
     pub Mark1 { // MarkRecord
         class_id      (u16), // markClass
         anchor_offset (u16), // markAnchorOffset
+
+        anchor (Anchor),
     }
 }
 
@@ -117,12 +121,8 @@ table! {
     pub Mark1s { // MarkArray
         count (u16), // markCount
 
-        records (Vec<Mark1>) |this, tape, _| { // markRecords
-            tape.take_given(this.count as usize)
-        },
-
-        anchors (Vec<Anchor>) |this, tape, position| {
-            jump_take!(tape, position, this.count, i => this.records[i].anchor_offset)
+        records (Vec<Mark1>) |this, tape, position| { // markRecords
+            (0..this.count).map(|_| tape.take_given(position)).collect::<Result<Vec<_>>>()
         },
     }
 }
@@ -132,6 +132,8 @@ table! {
     #[doc = "A mark attachment in format 2."]
     pub Mark2 { // Mark2Record
         anchor_offsets (Vec<u16>), // mark2AnchorOffsets
+
+        anchors (Vec<Option<Anchor>>),
     }
 }
 
@@ -141,8 +143,6 @@ table! {
     pub Mark2s { // Mark2Array
         count   (u16       ), // mark2Count
         records (Vec<Mark2>), // mark2Records
-
-        anchors (Vec<Vec<Option<Anchor>>>),
     }
 }
 
@@ -248,11 +248,16 @@ impl crate::Value for Anchor {
 }
 
 impl Walue<'static> for Base {
-    type Parameter = u16;
+    type Parameter = (u64, u16);
 
-    fn read<T: Tape>(tape: &mut T, mark_class_count: Self::Parameter) -> Result<Self> {
-        let anchor_offsets = tape.take_given(mark_class_count as usize)?;
-        Ok(Self { anchor_offsets })
+    fn read<T: Tape>(tape: &mut T, (position, mark_class_count): Self::Parameter) -> Result<Self> {
+        let anchor_offsets: Vec<u16> = tape.take_given(mark_class_count as usize)?;
+        let anchors =
+            tape.stay(|tape| jump_take_maybe!(tape, position, mark_class_count, anchor_offsets))?;
+        Ok(Self {
+            anchor_offsets,
+            anchors,
+        })
     }
 }
 
@@ -262,35 +267,24 @@ impl Walue<'static> for Bases {
     fn read<T: Tape>(tape: &mut T, mark_class_count: Self::Parameter) -> Result<Self> {
         let position = tape.position()?;
         let count = tape.take()?;
-        let mut records: Vec<Base> = Vec::with_capacity(count as usize);
-        for _ in 0..count {
-            records.push(tape.take_given(mark_class_count)?);
-        }
-        let mut anchors = Vec::with_capacity(count as usize);
-        #[allow(clippy::needless_range_loop)]
-        for i in 0..(count as usize) {
-            anchors.push(jump_take_maybe!(
-                @unwrap
-                tape,
-                position,
-                mark_class_count,
-                j => records[i].anchor_offsets[j]
-            ));
-        }
-        Ok(Self {
-            count,
-            records,
-            anchors,
-        })
+        let records = (0..count)
+            .map(|_| tape.take_given((position, mark_class_count)))
+            .collect::<Result<Vec<_>>>()?;
+        Ok(Self { count, records })
     }
 }
 
 impl Walue<'static> for Component {
-    type Parameter = u16;
+    type Parameter = (u64, u16);
 
-    fn read<T: Tape>(tape: &mut T, mark_class_count: Self::Parameter) -> Result<Self> {
-        let anchor_offsets = tape.take_given(mark_class_count as usize)?;
-        Ok(Self { anchor_offsets })
+    fn read<T: Tape>(tape: &mut T, (position, mark_class_count): Self::Parameter) -> Result<Self> {
+        let anchor_offsets: Vec<u16> = tape.take_given(mark_class_count as usize)?;
+        let anchors =
+            tape.stay(|tape| jump_take_maybe!(tape, position, mark_class_count, anchor_offsets))?;
+        Ok(Self {
+            anchor_offsets,
+            anchors,
+        })
     }
 }
 
@@ -298,11 +292,11 @@ impl Walue<'static> for Ligature {
     type Parameter = u16;
 
     fn read<T: Tape>(tape: &mut T, mark_class_count: Self::Parameter) -> Result<Self> {
+        let position = tape.position()?;
         let count = tape.take()?;
-        let mut records = Vec::with_capacity(count as usize);
-        for _ in 0..(count as usize) {
-            records.push(tape.take_given(mark_class_count)?);
-        }
+        let records = (0..count)
+            .map(|_| tape.take_given((position, mark_class_count)))
+            .collect::<Result<Vec<_>>>()?;
         Ok(Self { count, records })
     }
 }
@@ -314,40 +308,42 @@ impl Walue<'static> for Ligatures {
         let position = tape.position()?;
         let count = tape.take()?;
         let offsets: Vec<u16> = tape.take_given(count as usize)?;
-        let records: Vec<Ligature> =
-            jump_take_given!(@unwrap tape, position, count, offsets, mark_class_count);
-        let mut anchors = Vec::with_capacity(count as usize);
-        for i in 0..(count as usize) {
-            let mut inner = Vec::with_capacity(records[i].count as usize);
-            for j in 0..(records[i].count as usize) {
-                inner.push(jump_take_maybe!(
-                    @unwrap
-                    tape,
-                    position,
-                    mark_class_count,
-                    k => {
-                        let offset = records[i].records[j].anchor_offsets[k];
-                        if offset > 0 { offsets[i] + offset } else { 0 }
-                    }
-                ));
-            }
-            anchors.push(inner);
-        }
+        let records =
+            tape.stay(|tape| jump_take_given!(tape, position, count, offsets, mark_class_count))?;
         Ok(Self {
             count,
             offsets,
             records,
-            anchors,
+        })
+    }
+}
+
+impl Walue<'static> for Mark1 {
+    type Parameter = u64;
+
+    fn read<T: Tape>(tape: &mut T, position: Self::Parameter) -> Result<Self> {
+        let class_id = tape.take()?;
+        let anchor_offset = tape.take()?;
+        let anchor = tape.stay(|tape| jump_take!(tape, position, anchor_offset))?;
+        Ok(Self {
+            class_id,
+            anchor_offset,
+            anchor,
         })
     }
 }
 
 impl Walue<'static> for Mark2 {
-    type Parameter = u16;
+    type Parameter = (u64, u16);
 
-    fn read<T: Tape>(tape: &mut T, mark_class_count: Self::Parameter) -> Result<Self> {
-        let anchor_offsets = tape.take_given(mark_class_count as usize)?;
-        Ok(Self { anchor_offsets })
+    fn read<T: Tape>(tape: &mut T, (position, mark_class_count): Self::Parameter) -> Result<Self> {
+        let anchor_offsets: Vec<u16> = tape.take_given(mark_class_count as usize)?;
+        let anchors =
+            tape.stay(|tape| jump_take_maybe!(tape, position, mark_class_count, anchor_offsets))?;
+        Ok(Self {
+            anchor_offsets,
+            anchors,
+        })
     }
 }
 
@@ -356,27 +352,11 @@ impl Walue<'static> for Mark2s {
 
     fn read<T: Tape>(tape: &mut T, mark_class_count: Self::Parameter) -> Result<Self> {
         let position = tape.position()?;
-        let count = tape.take::<u16>()?;
-        let mut records: Vec<Mark2> = Vec::with_capacity(count as usize);
-        for _ in 0..count {
-            records.push(tape.take_given(mark_class_count)?);
-        }
-        let mut anchors = Vec::with_capacity(count as usize);
-        #[allow(clippy::needless_range_loop)]
-        for i in 0..(count as usize) {
-            anchors.push(jump_take_maybe!(
-                @unwrap
-                tape,
-                position,
-                mark_class_count,
-                j => records[i].anchor_offsets[j]
-            ));
-        }
-        Ok(Self {
-            count,
-            records,
-            anchors,
-        })
+        let count = tape.take()?;
+        let records = (0..count)
+            .map(|_| tape.take_given((position, mark_class_count)))
+            .collect::<Result<Vec<_>>>()?;
+        Ok(Self { count, records })
     }
 }
 
@@ -409,10 +389,9 @@ impl Walue<'static> for Pair1s {
     fn read<T: Tape>(tape: &mut T, (value1_flags, value2_flags): Self::Parameter) -> Result<Self> {
         let position = tape.position()?;
         let count = tape.take()?;
-        let mut records = Vec::with_capacity(count as usize);
-        for _ in 0..(count as usize) {
-            records.push(tape.take_given((position, value1_flags, value2_flags))?);
-        }
+        let records = (0..count)
+            .map(|_| tape.take_given((position, value1_flags, value2_flags)))
+            .collect::<Result<Vec<_>>>()?;
         Ok(Self { count, records })
     }
 }
@@ -446,10 +425,9 @@ impl Walue<'static> for Pair2s {
         tape: &mut T,
         (position, class2_count, value1_flags, value2_flags): Self::Parameter,
     ) -> Result<Self> {
-        let mut records = Vec::with_capacity(class2_count as usize);
-        for _ in 0..(class2_count as usize) {
-            records.push(tape.take_given((position, value1_flags, value2_flags))?);
-        }
+        let records = (0..class2_count)
+            .map(|_| tape.take_given((position, value1_flags, value2_flags)))
+            .collect::<Result<Vec<_>>>()?;
         Ok(Self { records })
     }
 }
